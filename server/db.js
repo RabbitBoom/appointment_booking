a21f0c5a5ed5ec64cf4967aa1eec6a2dadda8c73
RabbitBoom <276671662@qq.com>
2025-08-28 17:49:45 +0800

    update: update server config

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  `mongodb://root:${process.env.VITE_MONGO_PASSWORD}@${process.env.VITE_MONGO_HOST}:${process.env.VITE_MONGO_PORT}`;
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
